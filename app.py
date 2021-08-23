3c447b98775ac3e480c24b0898a08a8f5870c51f
Rudramani Singha <monacotime@outlook.com>
2021-08-23 17:05:14 +0530

    flasked

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_world():
+    return render_template("page.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002,debug=False)
+    
