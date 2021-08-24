fcbcc45f035b3ca0aa960a97d35c0309c059aac1
luckyCasualGuy <luckyCasualGuy@protonmail.com>
2021-08-24 05:16:30 -0700

    monday fix

--- static/js/main_monoco.js
+++ static/js/main_monoco.js
@@ -1,6 +1,28 @@
 import head_rotation from "./head_rotation.js"
+import TabChange from "./tabchange.js";
+let start_btn = document.getElementById('start');
 
-current_events = {"head_status": ""}
+
+function debug(out) {
+    let where = document.getElementById('debug_out');
+    let p = document.createElement('p');
+    p.innerHTML = JSON.stringify(out);
+    where.appendChild(p);
+    console.log(out)
+}
+let TC = new TabChange(debug);
+TC.set_start(start_btn);
+
+// ==========
+
+let current_events = {"head_status": ""}
+
+
+start_btn.addEventListener('click', event => {
+  // Start all your modules here <even the active tabs>
+  camera.start();
+  //active .start
+});
 
 const videoElement = document.getElementsByClassName('input_video')[0];
 
