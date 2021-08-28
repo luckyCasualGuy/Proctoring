3d6a1caf5ae99009f06d94092125a22355ffd35d
Rudramani Singha <monacotime@outlook.com>
2021-08-28 18:43:26 +0530

    new

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask
 from flask import render_template
+from flask_mysqldb import MySQL
 from werkzeug.datastructures import RequestCacheControl
 from flask import request, send_file
 
-from handler import data_handler
+import mysql.connector
+from mysql.connector import connection
+
+# from handler import data_handler
 
 app = Flask(__name__, static_url_path='/static',template_folder="static/templates")
+
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_DB'] = 'procter'
+
 app.config['DEBUG'] = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+mysql = MySQL(app)
+
+def log_to_db(data):
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT count(code) FROM user_docs WHERE code =%s",("hello",))
 
 @app.route("/", methods=['GET'])
 def hello_world_get(): return render_template("page.html")
@@ -17,7 +33,8 @@ def hello_world_post():
     user = 1
     session_name = "Sample MCQ Test"
     data = request.json
-    data_handler(data, user, session_name)
+    # data_handler(data, user, session_name)
+    # log_to_db(data)
     return {'comment': 'received'}
     
 
--- handler.py
+++ handler.py
@@ -7,22 +7,25 @@ from time import time
 USER_LOGS = Path('user logs')
 
 def data_handler(data: dict, user: int, session_name: str):
-    df = DataFrame([data])
+    # df = DataFrame([data])
 
-    user_path = USER_LOGS / str(user)
-    if not user_path.exists(): user_path.mkdir()
-    user_path = user_path / str(session_name)
-    if not user_path.exists(): user_path.mkdir()
+    # user_path = USER_LOGS / str(user)
+    # if not user_path.exists(): user_path.mkdir()
+    # user_path = user_path / str(session_name)
+    # if not user_path.exists(): user_path.mkdir()
 
-    files = list(user_path.glob('*'))
-    print(files)
-    csv_file = files[0] if files else None
-    print(csv_file)
-    if not csv_file:
-        temp_csv = f"{int(time())}.csv"
-        df.to_csv(user_path/ temp_csv)
-    else:
-        adf = read_csv(csv_file)
-        adf = adf.append(df)
-        adf.to_csv(csv_file)
-        print(adf)
+
+    # files = list(user_path.glob('*'))
+    # print(files)
+    # csv_file = files[0] if files else None
+    # print(csv_file)
+    # if not csv_file:
+    #     temp_csv = f"{int(time())}.csv"
+    #     df.to_csv(user_path/ temp_csv)
+    # else:
+    #     adf = read_csv(csv_file)
+    #     adf = adf.append(df)
+    #     adf.to_csv(csv_file)
+    #     print(adf)
+
+    pass
