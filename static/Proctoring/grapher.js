6326fcc09b24821fe97d7eb04c7bd235e6573b19
Rudramani Singha <monacotime@outlook.com>
2021-09-08 17:45:23 +0530

    dwd

--- static/Proctoring/grapher.js
+++ static/Proctoring/grapher.js
@@ -1,63 +1,103 @@
-function get_data_graph(data){
-    document.getElementById("d3-container").innerHTML = ""
-    var gr_data = []
-    for (var key in data){
-        console.log(key)
-        let t_dic = {}
-        t_dic["name"] = key
-        t_dic["score"] = data[key]
-        
-        gr_data.push(t_dic)
-    }
-    console.log(gr_data)
+// "CLIENT PAGE FOCUS LOST",
+// "LOOKING AWAY START",
+// "LOOKING DOWN START",
+// "MISSING PERSON START",
+// "TAB CHANGE INVISIBLE",
+// "ALT KEYPRESS DETECTED",
+// "KEY TRAPS",
+// "LEFT MOUSE TRAPS",
+// "PAGE LEAVE",
+// "RIGHT MOUSE TRAPS",
+// "WINDOWS KEYPRESS DETECTED",
 
-    const width = 900;
-    const height = 450;
-    const margin = { top: 50, bottom: 50, left: 50, right: 50 };
+const graph_data = {
+    labels: [
+        "CLIENT PAGE FOCUS LOST",
+        "LOOKING AWAY START",
+        "LOOKING DOWN START",
+        "MISSING PERSON START",
+        "TAB CHANGE INVISIBLE",
+        "ALT KEYPRESS DETECTED",
+        "KEY TRAPS",
+        "LEFT MOUSE TRAPS",
+        "PAGE LEAVE",
+        "RIGHT MOUSE TRAPS",
+        "WINDOWS KEYPRESS DETECTED",
+    ],
+    
+    datasets: [{
+        label: 'My Average Dataset',
+        data: [65, 59, 90, 81, 56, 55, 40, 81, 56, 55, 40],
+        fill: true,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgb(255, 99, 132)',
+        pointBackgroundColor: 'rgb(255, 99, 132)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgb(255, 99, 132)'
+    }]
+};
 
-    const svg = d3.select('#d3-container')
-        .append('svg')
-        .attr('width', width - margin.left - margin.right)
-        .attr('height', height - margin.top - margin.bottom)
-        .attr("viewBox", [0, 0, width, height]);
+const config = {
+    type: 'radar',
+    data: graph_data,
+    options: {
+        elements: {
+            line: {
+                borderWidth: 3
+            }
+        },
+        responsive: true,
+        plugins: {
+        title: {
+            display: true,
+            text: 'Chart.js Radar Chart'
+        }
+        }
+    },
+};
 
-    const x = d3.scaleBand()
-        .domain(d3.range(gr_data.length))
-        .range([margin.left, width - margin.right])
-        .padding(0.1)
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, config)
+console.log(myChart.data.datasets)
 
-    const y = d3.scaleLinear()
-        .domain([0, 100])
-        .range([height - margin.bottom, margin.top])
-
-    svg
-        .append("g")
-        .attr("fill", 'royalblue')
-        .selectAll("rect")
-        // .data(gr_data.sort((a, b) => d3.descending(a.score, b.score)))
-        .data(gr_data)
-        .join("rect")
-            .attr("x", (d, i) => x(i))
-            .attr("y", d => y(d.score))
-            .attr('title', (d) => d.score)
-            .attr("class", "rect")
-            .attr("height", d => y(0) - y(d.score))
-            .attr("width", x.bandwidth());
-
-    function yAxis(g) {
-        g.attr("transform", `translate(${margin.left}, 0)`)
-            .call(d3.axisLeft(y).ticks(null, gr_data.format))
-            .attr("font-size", '20px')
-    }
-
-    function xAxis(g) {
-        g.attr("transform", `translate(0,${height - margin.bottom})`)
-            .call(d3.axisBottom(x).tickFormat(i => gr_data[i].name))
-            .attr("font-size", '20px')
-    }
-
-    svg.append("g").call(xAxis);
-    svg.append("g").call(yAxis);
-    svg.node();
+var student_graph = {
+    label: 'My Student Dataset',
+    data: [],
+    fill: true,
+    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+    borderColor: 'rgb(54, 162, 235)',
+    pointBackgroundColor: 'rgb(54, 162, 235)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(54, 162, 235)'
 }
 
+function change_graph(stu_data){
+    
+    const values = []
+    for(value in stu_data){
+        values.push(stu_data[value])
+    }
+
+    student_graph.data = values
+
+    // var myChart = new Chart(ctx, config)
+
+    console.log(myChart.data.datasets)
+
+    myChart.data.datasets[1] = student_graph
+
+    myChart.update()
+}
+
+function change_graph_old(){
+
+    // var myChart = new Chart(ctx, config)
+
+    console.log(myChart.data.datasets)
+
+    myChart.data.datasets[1] = student_graph
+
+    myChart.update()
+}
