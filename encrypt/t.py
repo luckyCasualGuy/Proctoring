76e8db39bec6bc5e1610f2b0525e31d9ce2a9af0
luckyCasualGuy <luckyCasualGuy@protonmail.com>
2021-09-15 04:57:57 -0700

    encrypt

--- encrypt/t.py
+++ encrypt/t.py
@@ -8,6 +8,7 @@ class Encrypt:
     __key_length = 32
     __key_dir = Path("./encrypt/KEY")
     __key_path = __key_dir / "K04211409.bin"
+    __encryption_dir = Path('./encrypt/encyptions')
 
     def generate_key(self):
         key = get_random_bytes(self.__key_length)
@@ -18,6 +19,21 @@ class Encrypt:
         self.__key_path.write_bytes(key)
         print(f"key stored in {self.__key_path.absolute()}")
 
+    def read_key(self): return self.__key_path.read_bytes()
 
-    def reead_key(self):
-        return self.__key_path.read_bytes()
+    def encrypt(self, key, data):
+        cipher = AES.new(key, AES.MODE_EAX)
+        ciphered_data, tag = cipher.encrypt_and_digest(data)
+        return cipher.nonce, tag, ciphered_data
+
+    def decrypt(self, key, nonce, tag, data):
+        cipher = AES.new(key, AES.MODE_EAX, nonce)
+        return cipher.decrypt_and_verify(data, tag)
+
+    def save(self, roll_no, session, nonce, tag, data):
+        en_path = self.__encryption_dir / session / roll_no
+        file_out = open(en_path, "wb")
+        file_out.write(nonce) # Write the nonce to the output file (will be required for decryption - fixed size)
+        file_out.write(tag) # Write the tag out after (will be required for decryption - fixed size)
+        file_out.write(data)
+        file_out.close()
